4c8fec0f3178e48c8fe53e778f53b5af6dd07d12
subhash9758964966 <subhashsagar98@gmail.com>
2023-01-01 12:03:41 +0530

    add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src",
+    version= "0.0.1",
+    description= "its a wineq packege",
+    author="subh98",
+    packages=find_packages()
+)
---
+++ test/test_config.py
@@ -0,0 +1,4 @@
+def test_generic():
+    a=1
+    b=1
+    assert a == b
